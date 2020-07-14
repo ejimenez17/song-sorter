fa546424d196b065d5ff5cb2b6f2fbca9864f3ce
ejimenez17 <ejimenez@g.hmc.edu>
2020-07-14 15:14:34 -0700

    implemented sort method

---
+++ components/linkedlist.js
@@ -0,0 +1,90 @@
+// LinkedList class
+
+export default class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+export default class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    add(element) {
+        if (this.head == null) {
+            this.head = element;
+        } else {
+            var currNode = this.head;
+
+            while (currNode.value != element) {
+                currNode = currNode.next;
+            }
+
+            currNode.next = new ListNode(element);
+        }
+
+        this.size++;
+    }
+
+    get(i) {
+        if (i >= this.size) {
+            throw "Index out of bounds";
+        } else {
+            let count = 0; 
+            let currNode = this.head;
+
+            while (count < i) {
+                currNode = currNode.next;
+                count++;
+            }
+
+            return currNode;
+        }
+    }
+
+    isEmpty() {
+        return (this.size == 0 && this.head == null);
+    }
+
+    insert(element, index, addToIndex) {
+        if (this.size < index) {
+            throw "Index out of bounds";
+        } else if (this.size == index || this.head == null) {
+            this.add(element);
+        } else {
+            let count = 0;
+            let currNode = this.head;
+
+            while (count < index) {
+                currNode = currNode.next;
+                count++;
+            }
+
+            if (addToIndex) { // add element to current index
+                let newNodeList;
+                if (Array.isArray(currNode.value)) {
+                    newNodeList = currNode.value;
+                    newNodeList.add(element);
+                } else {
+                    newNodeList = new Array(2);
+                    newNodeList[0] = currNode.value;
+                    newNodeList[1] = element;
+                    currNode.value = newNodeList;
+                }
+            } else { // make new node at this index
+                let newNodeNext = new ListNode(currNode.value);
+                newNodeNext.next = currNode.next;
+                currNode.value = element;
+                currNode.next = newNodeNext;
+            }
+            this.size++;
+        }
+    }
+
+    size() {
+        return this.size;
+    }
+}
--- pages/sort.js
+++ pages/sort.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import { withRouter } from 'next/router'
+import {LinkedList, ListNode} from '../components/linkedlist'
 
 class SortBox extends React.Component {
     constructor(props) {
@@ -20,55 +20,117 @@ class SortBox extends React.Component {
                 "snap shoot",
                 "happy ending",
             ],
+            userPref = null,
         };
         this.preference = {
             LEFT: 'left',
             RIGHT: 'right',
-            BOTH: 'both',
-            NEITHER: 'neither'
+            BOTH: 'both'
+            // NEITHER: 'neither'
         };
     }
 
     // handleUserChoice:
     // change state values for boxes
     handleUserChoice(leftSong, rightSong) {
-        // set left song to left box
-        // set right song to right box
-        // promiseDoWhilst(() => {
-        //     // get user pref
-        // }, () => {
-        //     // if userPref == null
-        // }).then(() => {
-        //     // grab user pref and set state value to null again
-        //     // return user pref to advance sort alg
-        // });
+        // TODO: set left song to left box
+        // TODO: set right song to right box
+        promiseDoWhilst(() => {
+            // wait for user click
+            // boxes will have onClick methods to call that will set userPref
+        }, () => {
+            return userPref == null;
+        }).then(() => {
+            // grab user pref and set state value to null again
+            let currentUserPref = this.state.userPref;
+            this.state.userPref = null;
+            // return user pref to advance sort alg
+            return currentUserPref;
+        });
     }
 
     // when a song box is clicked - change state value for user click
-    // handleLeftClick();
+    handleLeftClick() {
+        this.userPref = this.preference.LEFT;
+    }
     // handleRightClick();
     // handleBothNeitherClick();
 
     insertSort(){
-        // let sortedArr = linked list (1D) for now
-        for (song in this.state.songs) {
-            // binary search thru sortedArr to find spot for songs
-                // start, end, insertIndex
-                // while start <= end
-                    // when start = end, insert song at insertIndex (start and end)
-                    // let userPref = handleUserChoice(song, sortedArr[insertIndex])
-                    // switch(userPref) {
-                    //     case this.preference.LEFT:
-                    //         // code - shift indexes accordingly
-                    //     case this.preference.RIGHT:
-                    //         // code - shift indexes accordingly
-                    //     case this.preference.BOTH:
-                    //         // code - shift indexes accordingly
-                    //     case this.preference.NEITHER:
-                    //         // code - shift indexes accordingly
-                    // }
+        let sortedList = new LinkedList();
+        for (songToPlace in this.state.songs) {
+            let start = 0;
+            // let end = this.state.songs.length;
+            let end = sortedList.size();
+            let insertIndex = (end - start) / 2 + start;
+            while (start <= end) {
+                // if (start == end) { // don't need this after all bc should compare song w/song @ insertIndex
+                //     // get user response and insert accordingly
+                //     sortedList.insertAt(songToPlace, insertIndex, false);
+                //     break;
+                // } else {
+                // current song is always on LEFT
+                let userPref = handleUserChoice(songToPlace, sortedArr.get(insertIndex));
+                switch(userPref) {
+                    case this.preference.LEFT:
+                        // code - shift indexes accordingly
+                        if (start == end) {
+                            sortedList.insertAt(songToPlace, insertIndex, false);
+                            break;
+                        }
+                        end = insertIndex - 1;
+                    case this.preference.RIGHT:
+                        // code - shift indexes accordingly
+                        if (start == end) {
+                            sortedList.insertAt(songToPlace, insertIndex+1, false);
+                            break;
+                        }
+                        start = insertIndex + 1;
+                    case this.preference.BOTH:
+                        // code - insert here addToIndex = true
+                        sortedList.insertAt(songToPlace, insertIndex, true);
+                        break;
+                    // case this.preference.NEITHER:
+                        // code - insert here addToIndex = true
+                }
+                insertIndex = (end - start) / 2 + start;
+                // }
+            }
         }
     }
+
+    // mergesort(arr) {
+    //     if (arr.length == 1) {
+    //         return arr;
+    //     }
+
+    //     const half = arr.length / 2;
+    //     let leftHalf = this.mergesort(arr.slice(0, half));
+    //     let rightHalf = this.mergesort(arr.slice(half, arr.length));
+    //     let leftP, rightP = 0;
+
+    //     let mergedArr = new Array(leftHalf.length + rightHalf.length).fill(null);
+    //     let insertIndex = 0;
+
+    //     while (insertIndex < mergedArr.length) {
+    //         if (leftP < leftHalf.length && rightP < rightHalf.length) {
+    //             let leftSong = leftHalf[leftP];
+    //             let rightSong = rightHalf[rightP];
+    //             let userPref = handleUserChoice(leftSong, rightSong);
+
+    //             switch(userPref) {
+    //                 case this.preference.LEFT:
+    //                     // code - shift indexes accordingly
+    //                 case this.preference.RIGHT:
+    //                     // code - shift indexes accordingly
+    //                 case this.preference.BOTH:
+    //                     // code - shift indexes accordingly
+    //                 case this.preference.NEITHER:
+    //                     // code - shift indexes accordingly
+    //             }
+    //         }
+    //     }
+    // }
 
     render() {
         const msg = "Pick songs for " + this.state.artist;
